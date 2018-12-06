914c595caa4f35eb597af715b5fae15cd518e0d1
Mạnh Trương <manhtruong531010@gmail.com>
2018-12-06 13:12:49 +0700

    Ceate

---
+++ app/src/main/java/com/team02/rentaccommodation/rentaccommodation/EditAccommodation.java
@@ -0,0 +1,13 @@
+package com.team02.rentaccommodation.rentaccommodation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EditAccommodation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_edit_accommodation );
+    }
+}
---
+++ app/src/main/java/com/team02/rentaccommodation/rentaccommodation/PostAccommodation.java
@@ -0,0 +1,13 @@
+package com.team02.rentaccommodation.rentaccommodation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PostAccommodation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_post_accommodation );
+    }
+}
